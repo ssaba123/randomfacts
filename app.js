941eba3e53076227eaa57eec179a092d20c7bd58
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-11-03 18:45:47 -0400

    Add tweet button

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fact = document.querySelector('.fact')
 const qAuthor = document.querySelector('.author')
 const button = document.querySelector('.btn')
+const tweet = document.querySelector('.twitter')
 
 const getQuote = async ()=>{
     const response = await fetch('https://api.quotable.io/random')
@@ -8,7 +9,9 @@ const getQuote = async ()=>{
     return json
 }
 
-
+tweet.addEventListener('click', ()=>{
+  console.log('clicked!!')
+})
 
 button.addEventListener('click', ()=>{
    getQuote().then(quotes => {
